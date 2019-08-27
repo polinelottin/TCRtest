7a2d40b6b2b5020964cfdabfda539095889aa023
Poline Lottin <lottinp@gmail.com>
2019-08-27 12:29:14 -0300

    TCR working $(date) @ $(hostname)

--- script.sh
+++ script.sh
@@ -1,30 +1,20 @@
-#!/bin/bash
-
-# fswatch -0 . | while read -d "" event; \
-# do \
-#    echo $(git status);
-#    echo hellooooo ${event};
-# done
-
 fswatch -0 $@ -e ".*" -i "\\.py$" | while read -d "" event; \
 do \
-   #echo hellooooo ${event};
     pytest;
-
-    #echo result is $?;
 
     if [ $? = 0 ]
     then 
-        echo $(git status); 
-        echo '';   
-        echo "Passou, vou adicionar tudo";
+        # echo $(git status); 
+        # echo ''; 
+
+        echo "IHU! Passou, bora commitar!";
         git add -A;
 
 
-        echo $(git status); 
-        echo '';
-        echo "nhaaa! Comitando"
-        git commit -m 'TCR working $(date) @ $(hostname)'
+        # echo $(git status); 
+        # echo '';
+
+        git commit -m 'TCR working $date @ $hostname';
         
 
         echo $(git status);
@@ -35,9 +25,3 @@ do \
         echo $(git status);
     fi
 done
-
-
-#fswatch -0 . | xargs -0 -n 1 -I 'git status'  
-
-# fswatch . | echo 'git status'
-#pytest
